b98371472bc477e104e7896c81438d31f91a993b
CristhianMotoche <cristhian.motoche@gmail.com>
2016-08-30 18:38:58 -0500

    Add Keys module

---
+++ src/Test/WebDriver/Common/Keys.hs
@@ -0,0 +1,198 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Test.WebDriver.Common.Keys where
+
+import Data.Text (Text)
+
+add_ :: Text
+add_ = "\xe025"
+
+alt_ :: Text
+alt_ = "\xe00a"
+
+arrowDown_ :: Text
+arrowDown_ = "\xe015"
+
+arrowLeft_ :: Text
+arrowLeft_ = "\xe012"
+
+arrowRight_ :: Text
+arrowRight_ = "\xe014"
+
+arrowUp_ :: Text
+arrowUp_ = "\xe013"
+
+backspace_ :: Text
+backspace_ = "\xe003"
+
+backSpace_ :: Text
+backSpace_ = "\xe003"
+
+cancel_ :: Text
+cancel_ = "\xe001"
+
+clear_ :: Text
+clear_ = "\xe005"
+
+command_ :: Text
+command_ = "\xe03d"
+
+control_ :: Text
+control_ = "\xe009"
+
+decimal_ :: Text
+decimal_ = "\xe028"
+
+delete_ :: Text
+delete_ = "\xe017"
+
+divide_ :: Text
+divide_ = "\xe029"
+
+down_ :: Text
+down_ = "\xe015"
+
+end_ :: Text
+end_ = "\xe010"
+
+enter_ :: Text
+enter_ = "\xe007"
+
+equals_ :: Text
+equals_ = "\xe019"
+
+escape_ :: Text
+escape_ = "\xe00c"
+
+f1_ :: Text
+f1_ = "\xe031"
+
+f2_ :: Text
+f2_ = "\xe032"
+
+f3_ :: Text
+f3_ = "\xe033"
+
+f4_ :: Text
+f4_ = "\xe034"
+
+f5_ :: Text
+f5_ = "\xe035"
+
+f6_ :: Text
+f6_ = "\xe036"
+
+f7_ :: Text
+f7_ = "\xe037"
+
+f8_ :: Text
+f8_ = "\xe038"
+
+f9_ :: Text
+f9_ = "\xe039"
+
+f10_ :: Text
+f10_ = "\xe03a"
+
+f11_ :: Text
+f11_ = "\xe03b"
+
+f12_ :: Text
+f12_ = "\xe03c"
+
+help_ :: Text
+help_ = "\xe002"
+
+home_ :: Text
+home_ = "\xe011"
+
+insert_ :: Text
+insert_ = "\xe016"
+
+left_ :: Text
+left_ = "\xe012"
+
+leftAlt_ :: Text
+leftAlt_ = "\xe00a"
+
+leftControl_ :: Text
+leftControl_ = "\xe009"
+
+leftShift_ :: Text
+leftShift_ = "\xe008"
+
+meta_ :: Text
+meta_ = "\xe03d"
+
+multiply_ :: Text
+multiply_ = "\xe024"
+
+null_ :: Text
+null_ = "\xe000"
+
+numpad0_ :: Text
+numpad0_ = "\xe01a"
+
+numpad1_ :: Text
+numpad1_ = "\xe01b"
+
+numpad2_ :: Text
+numpad2_ = "\xe01c"
+
+numpad3_ :: Text
+numpad3_ = "\xe01d"
+
+numpad4_ :: Text
+numpad4_ = "\xe01e"
+
+numpad5_ :: Text
+numpad5_ = "\xe01f"
+
+numpad6_ :: Text
+numpad6_ = "\xe020"
+
+numpad7_ :: Text
+numpad7_ = "\xe021"
+
+numpad8_ :: Text
+numpad8_ = "\xe022"
+
+numpad9_ :: Text
+numpad9_ = "\xe023"
+
+pageDown_ :: Text
+pageDown_ = "\xe00f"
+
+pageUp_ :: Text
+pageUp_ = "\xe00e"
+
+pause_ :: Text
+pause_ = "\xe00b"
+
+return_ :: Text
+return_ = "\xe006"
+
+right_ :: Text
+right_ = "\xe014"
+
+semicolon_ :: Text
+semicolon_ = "\xe018"
+
+separator_ :: Text
+separator_ = "\xe026"
+
+shift_ :: Text
+shift_ = "\xe008"
+
+space_ :: Text
+space_ = "\xe00d"
+
+subtract_ :: Text
+subtract_ = "\xe027"
+
+tab_ :: Text
+tab_ = "\xe004"
+
+up_ :: Text
+up_ = "\xe013"
+
